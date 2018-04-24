c8e6885d9a85ea6c4cf5f2bacea63e0c2b54b126
QL Student <qlstudent@protonmail.com>
2018-04-24 11:30:36 -0400

    use variable names for sitemap xml

--- index.php
+++ index.php
@@ -2,10 +2,24 @@
 header('Content-Type: text/csv');
 header('Content-Disposition: attachment; filename="sitemap.csv"');
 $locations = ["/bib/1", "/bib/2", "/bib/3", "/bib/4", "/bib/5"];
+$id = 1;
+$type ="custom";
+$subtype ="";
+$language = "en";
+$access = 1;
+$status = 1;
+$status_override = 0;
+$lastmod = time(); 
 $priority = "0.5";
+$priority_override = 0;
+$changefreq = "86400";
+$changecount = 0;
 $sravan = array();
 foreach($locations as $location){
-    $sravan[] = $location . "," . $priority . ",potato,tomato";
+    $sravan[] = $id . "," . $type . "," . $subtype . "," . $location . "," . $language 
+    . "," . $access . "," . $status . "," . $status_override . "," . $lastmod . "," 
+    . $priority . "," . $priority_override . "," . $changefreq . "," . $changecount;
+    $id += 1;
 }
 
 $fp = fopen('php://output', 'w');
